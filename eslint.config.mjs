ade03bebac8492d451a83298d833404ca9dc4702
Lorenzo Monacò <lorenzo@ironkernel.net>
2026-09-21 01:12:45 +0200

    add: added eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores([
+    "out/",
+    ".test-out/",
+    ".vscode-test/",
+    ".worktrees/",
+    "node_modules/",
+  ]),
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
 ]);
